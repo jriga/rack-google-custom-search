6315c1dd2cc7c0922e3356761d9ab812de1ee856
Jerome Riga <jriga@zemis.co.uk>
2010-11-25 12:11:24 +0000

    added support for auto_complete options

--- lib/rack/google-custom-search.rb
+++ lib/rack/google-custom-search.rb
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 module Rack #:nodoc:
   class GoogleCustomSearch < Struct.new :app, :options
-
+    DEFAULTS_OPTIONS = {:css_id => 'cse', :auto_complete => false}
     def call(env)
       status, headers, response = app.call(env)
-
+      
       if headers["Content-Type"] =~ /text\/html|application\/xhtml\+xml/
         body = ""
         response.each { |part| body << part }
         index = body.rindex("</body>")
         if index
-          body.insert(index, js_files(options[:token]))
+          body.insert(index, js_files(DEFAULTS_OPTIONS.merge(options)))
           headers["Content-Length"] = body.length.to_s
           response = [body]
         end
@@ -20,20 +21,26 @@ module Rack #:nodoc:
 
     protected
 
-      # Returns JS to be embeded. 
-      def js_files(token)
-        returning_value = <<-EOF
+    # Returns JS to be embeded. 
+    def js_files(options={})
+      returning_value = <<-EOF
 <script src="http://www.google.com/jsapi" type="text/javascript"></script>
 <script type="text/javascript">
-  google.load('search', '1');
-  google.setOnLoadCallback(function() {
-    var customSearchControl = new google.search.CustomSearchControl('#{token}');
-    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-    customSearchControl.draw('cse');
-  }, true);
+  google.load('search', '1');
+  google.setOnLoadCallback(function() {
+    var customSearchControl = new google.search.CustomSearchControl('#{options[:token]}');
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    #{auto_complete(options[:auto_complete])}
+    customSearchControl.draw('#{options[:css_id]}', options);
+  }, true);
 </script>
 EOF
       returning_value
-      end
+    end
+    
+    def auto_complete(cond)
+      'options.setAutoComplete(true);' if cond
+    end
   end
 end
--- spec/rack-google-custom-search_spec.rb
+++ spec/rack-google-custom-search_spec.rb
@@ -1,7 +1,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Rack::GoogleCustomSearch" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before { @midddleware = Rack::GoogleCustomSearch.new(nil, {}) }
+
+  it "should add autocompletion when set to true" do
+    expected = <<-EOF
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<script type="text/javascript">
+  google.load('search', '1');
+  google.setOnLoadCallback(function() {
+    var customSearchControl = new google.search.CustomSearchControl('token');
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    options.setAutoComplete(true);
+    customSearchControl.draw('css_id', options);
+  }, true);
+</script>
+EOF
+    
+    actual_result = @midddleware.send(:js_files, {:css_id => 'css_id', :token => 'token', :auto_complete => true})
+    actual_result.should == expected
+  end
+
+  it "should return default" do
+    expected = <<-EOF
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<script type="text/javascript">
+  google.load('search', '1');
+  google.setOnLoadCallback(function() {
+    var customSearchControl = new google.search.CustomSearchControl('token');
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    
+    customSearchControl.draw('css_id', options);
+  }, true);
+</script>
+EOF
+    
+    actual_result = @midddleware.send(:js_files, {:css_id => 'css_id', :token => 'token', :auto_complete => false})
+    actual_result.should == expected
   end
 end
